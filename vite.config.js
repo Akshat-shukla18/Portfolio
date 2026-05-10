a6cc2a299c923b88c6ee86d3d36a913cd5e6df01
Akshat Shukla <ashukla20062006@gmail.com>
2026-05-10 13:08:45 +0530

    fix: remove unused path alias configuration from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "next/image": path.resolve(__dirname, 'empty-module.js')
+
+  build: {
+    target: 'esnext'
+  },
+
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
     }
   }
 })
